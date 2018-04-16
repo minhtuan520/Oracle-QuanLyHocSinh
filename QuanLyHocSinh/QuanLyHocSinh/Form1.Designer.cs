f2dff5cdedc4566d3293084ba2bae8f60d17b70f
minhtuan520 <minhtuan520@hotmail.com.vn>
2018-04-16 17:36:34 +0700

    create project

---
+++ QuanLyHocSinh/QuanLyHocSinh/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace QuanLyHocSinh
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gridview = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.gridview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gridview
+            // 
+            this.gridview.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridview.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridview.Location = new System.Drawing.Point(0, 0);
+            this.gridview.Name = "gridview";
+            this.gridview.Size = new System.Drawing.Size(738, 367);
+            this.gridview.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(738, 367);
+            this.Controls.Add(this.gridview);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gridview)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView gridview;
+    }
+}
+
---
+++ QuanLyHocSinh/QuanLyHocSinh/Form1.cs
@@ -0,0 +1,27 @@
+﻿using QuanLyHocSinh.DAL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyHocSinh
+{
+    public partial class Form1 : Form
+    {
+        QuanLyHocSinhEntities QuanLyHocSinh = new QuanLyHocSinhEntities();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            gridview.DataSource = QuanLyHocSinh.TYPEACCOUNT.ToList();
+        }
+    }
+}
